22e2061256294e3db8169d328e55d94d7319ccdb
Nata Kukushkina <n.kukushkina@gmail.com>
2012-09-10 16:47:25 +0300

    1st Commit from ecosoft office

---
+++ src/ServerSide.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerSide 
+{
+	public static void main(String[] args)
+	{
+		try 
+		{
+			ServerSocket serverSocket = new ServerSocket(1234);
+			Socket incoming = serverSocket.accept();
+			
+			try 
+			{
+				InputStream inStream = incoming.getInputStream();
+				OutputStream outStream = incoming.getOutputStream();
+				
+				Scanner in = new Scanner(inStream);
+				PrintWriter out = new PrintWriter(outStream, true);
+				
+				out.println("Hello! Enter EXIT to exit.");
+				
+				boolean done = false;
+				while (!done && in.hasNextLine())
+				{
+					String line = in.nextLine();
+					out.println("Echo: " + line);
+					if (line.trim().equals("EXIT")) done = true;
+				}
+			} 
+			finally 
+			{
+				incoming.close();
+			}
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
