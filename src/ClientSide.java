339f4fe22ea44167d7b008b223a9f7cf19f0d3db
Nata Kukushkina <n.kukushkina@gmail.com>
2012-10-09 17:43:31 +0300

    ClientSide changes

--- src/ClientSide.java
+++ src/ClientSide.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.Receiver;
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,41 +13,109 @@ public class ClientSide
     public ClientSide(String host, int port) throws IOException
     {
         s = new Socket(host, port);
+        socketReader = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+        socketWriter = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"));
+        userInput = new BufferedReader(new InputStreamReader(System.in));
+        new Thread(new Receiver()).start();
     }
+
+    public void run()
+    {
+        System.out.println("Type phrase(s) (hit Enter to exit):");
+        while (true)
+        {
+            String userString = null;
+            try{
+                userString = userInput.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            if (userString == null || userString.length() == 0 || s.isClosed())
+            {
+                close();
+                break;
+            }
+            else
+            {
+                try
+                {
+                    socketWriter.write("\n");
+                    socketWriter.flush();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                    close();
+                }
+            }
+
+        }
+    }
+
+    public synchronized void close()
+    {
+        if(!s.isClosed())
+        {
+            try
+            {
+                s.close();
+                System.exit(0);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
     public static void main(String[] args)
     {
         try
         {
-            if (args.length != 2)
-                System.out.println("Wrong number of arguments.");
-            String host = args[0];
-            int port = Integer.parseInt(args[1]);
-
-            Socket connecting = new Socket(host, port);
-
-            Scanner inFromServer = new Scanner(connecting.getInputStream());
-            PrintWriter outToServer = new PrintWriter(connecting.getOutputStream(), true);
-            Scanner fromConsol = new Scanner(System.in);
-
-            String fromUser, fromServer;
-            boolean done = false;
-            while (!done && fromConsol.hasNextLine())
-            {
-                fromUser = fromConsol.nextLine();
-                outToServer.println(fromUser);
-                fromServer = inFromServer.nextLine();
-                System.out.println(fromServer);
-                if (fromUser.trim().equals("BYE"))
-                {
-                    done = true;
-                }
-            }
-
-            connecting.close();
+            new ClientSide("127.0.0.1", 1234).run();
         }
         catch (IOException e)
         {
             e.printStackTrace();
+            System.out.println("Unable to connect. Server is not running?");
+        }
+    }
+
+    private class Receiver implements Runnable
+    {
+        public void run()
+        {
+            while (!s.isClosed())
+            {
+                String line = null;
+                try
+                {
+                    line = socketReader.readLine();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                    if ("Socket closed".equals(e.getMessage()))
+                    {
+                        break;
+                    }
+                    System.out.println("Connection lost");
+                    close();
+                }
+
+                if (line == null)
+                {
+                    System.out.println("Server has closed connection.");
+                    close();
+                }
+                else
+                {
+                    System.out.println("Server: " + line);
+                }
+            }
         }
     }
 }
